902f7f9b8f70ee38a38a0d7c022d2ee27fa6a35b
Nazarko12 <nazartsiupryk@gmail.com>
2021-11-20 16:18:33 +0200

    Added directories and files representing work with BLoC and asynchronous code

--- lib/main.dart
+++ lib/main.dart
@@ -1,185 +1,53 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_news_app/src/bloc/simple_bloc_delegate.dart';
+import 'package:flutter_news_app/src/commonWidget/bloc/bloc.dart';
+import 'package:flutter_news_app/src/theme/bloc/theme_bloc.dart';
+import 'package:flutter_news_app/src/theme/bloc/theme_state.dart';
+import 'package:flutter_news_app/src/theme/theme.dart';
+import 'src/helpers/routes.dart';
+import 'src/pages/homePage/bloc/bloc.dart';
+import 'src/pages/newsDetail/bloc/bloc.dart';
+import 'src/resources/repository.dart';
 
 void main() {
+  BlocObserver observer = SimpleBlocObserver();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
-      home: PersonWidget(),
-    );
-  }
-}
-
-class FavoriteWidget extends StatefulWidget {
-
-  @override
-  _FavoriteWidgetState createState() => _FavoriteWidgetState();
-}
-
-class _FavoriteWidgetState extends State<FavoriteWidget> {
-  bool _isFavorited = false;
-  int _favoriteCount = 87654;
-
-  @override
-  Widget build(BuildContext context){
-    return Row(
-      children: <Widget>[
-        Container(
-          child: IconButton(
-            icon: (_isFavorited ? Icon(Icons.favorite) : Icons.favorite_border),
-            onPressed: _toggleFavorite,
-            color: Colors.red[500],
-          ),
+    ThemeData apptheme;
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<NewsBloc>(
+          create: (context) =>
+              NewsBloc(repository: Repository())..add(Fetch(type: 'General')),
         ),
-          SizedBox(
-            width: 45,
-            child: Container(
-            child: Text('$_isFavorited'),
-
-          ),
-        )
+        BlocProvider<DetailBloc>(create: (context) => DetailBloc()),
+        BlocProvider<NavigationBloc>(create: (context) => NavigationBloc()),
+        BlocProvider<ThemeBloc>(create: (context) => ThemeBloc()),
       ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          if (state is SelectedTheme) {
+            apptheme = state.themeType != ThemeType.Light
+                ? AppTheme.lightTheme
+                : AppTheme.darkTheme;
+          }
+          return Builder(
+            builder: (context) {
+              return MaterialApp(
+                title: 'Flutter Demo',
+                theme: apptheme,
+                debugShowCheckedModeBanner: false,
+                routes: Routes.getRoute(),
+              );
+            },
+          );
+        },
+      ),
     );
   }
-  void _toggleFavorite() {
-    setState(() {
-
-      if (_isFavorited) {
-        _isFavorited = false;
-        _favoriteCount -= 1;
-
-      } else {
-          _isFavorited = true; 
-          _favoriteCount += 1;
-      }
-
-      
-    });
-  }
-  
 }
-
-class PersonWidget extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text('Football palyer'),
-        ),
-        body: Container(
-          child: _buildMainColumn()
-        ),
-      );
-    }
-
-    Widget _buildMainColumn() => ListView(
-      children: [
-        _buildTopImage(),
-        Center(child: Container(
-          padding: const EdgeInsets.only(left: 20, right: 20),
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(5),
-                child: _buildRating(),
-                 ),
-                 Card(
-                   elevation: 5,
-                   child: Container(
-                     margin: const EdgeInsets.all(5),
-                     padding: const EdgeInsets.all(10),
-                     child: _buildAction(),
-                   ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.all(5),
-                    child: _buildDescription(),
-                 ),
-                ],
-              ),
-            ))
-          ]
-        );
-
-        Widget _buildTopImage() => Container(
-          child: Card(
-            margin: EdgeInsets.only(left: 20,
-             right: 20,
-             top: 20
-             ),
-            elevation: 5,
-            child: Image.asset(
-              'assets/images/', 
-              fit: BoxFit.cover,
-              ),
-            ),
-          );
-
-          Widget _buildRating() => ListTile(
-            title: Text(
-              'Marco Veratti',
-              style: TextStyle(
-                fontWeight: FontWeight.w500,
-                ),
-              ),
-              subtitle: Text(
-                'Football club PSG',
-              ),
-              trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: <Widget>[
-                  FavoriteWidget()
-                ],
-              ),
-           
-          );
-
-          Widget _buildAction() => Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              _buildButton('Quantity', Icons.star, Colors.grey),
-              _buildButton('Position', Icons.star, Colors.grey),
-              _buildButton('National', Icons.star, Colors.grey),
-            ],
-          );
-
-          Widget _buildButton(
-            String label,
-            IconData icon,
-            Color color
-            ) => Column(
-            children: <Widget>[
-              Icon(icon, color: Colors.grey,),
-              Container(
-                child: Text(
-                  label,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w400,
-                    color: color,
-                  ),
-                ),
-              ),
-            ],
-          );
-
-          Widget _buildDescription() => Text(
-            ' ',
-            softWrap: true,
-            style: TextStyle(
-              fontSize: 15,
-            ),
-          );
-
-     }
-
-
-
-   
